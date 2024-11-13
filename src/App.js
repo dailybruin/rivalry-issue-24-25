ca64fc1fdf4b9bb6a8cc32d31cbafc5ff00ec4f6
tsakshi01 <tsakshi01@gmail.com>
2024-11-12 16:59:47 -0800

    Fixed AML link

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [ data, setData ] = useState(null);
   
   useEffect(() => {
-		fetch("<TODO: insert api url here>")
+		fetch("https://kerckhoff.dailybruin.com/api/packages/flatpages/rivalry-issue-24-25")
 		.then(res => res.json())
 		.then(res => setData(res.data['article.aml']))
   }, [])
