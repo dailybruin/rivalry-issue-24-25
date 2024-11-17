af4c61c84749d4ab9b0cd3b19ca1bbdef483fe20
maxfukuh4ra <maxfuku04@g.ucla.edu>
2024-11-17 13:08:46 -0800

    randomized bg color

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,6 +1,13 @@
+import React from "react";
 import styled from "styled-components";
 
-const ArticleContainer = styled("div")` // outermost container
+const colors = ["#28a745", "#E1823E", "#007BFF", "#20c997", "#ffc107", "#ff66cc"]; 
+
+const getRandomColor = () => {
+  return colors[Math.floor(Math.random() * colors.length)];
+};
+
+const ArticleContainer = styled("div")`
   position: relative;
   display: flex;
   flex-direction: row;
@@ -11,27 +18,25 @@ const ArticleContainer = styled("div")` // outermost container
   width: 212.199px;
   height: 305.061px;
   flex-shrink: 0;
-  background: #E1823E;
-  transition: box-shadow 0.3s ease-in-out; /* smooth transition for hover effect */
+  background: ${(props) => props.bgColor}; /* random background color passed as prop */
+  transition: box-shadow 0.3s ease-in-out;
 
+  &:hover {
+    box-shadow: 0 0 30px 15px rgba(0, 0, 0, 0.4); /* blurred shadow effect */
+  }
 
   @media (max-width: 900px) {
     flex-direction: column;
     align-items: center;
     text-align: center;
   }
-
-  &:hover {
-    box-shadow: 0 0 30px 15px rgba(0, 0, 0, 0.4); /* More noticeable shadow blur */
-  }
 `;
 
-const BoxContainer = styled("div")` // inner, black box container that contains the title
+const BoxContainer = styled("div")`
   position: relative;
   display: flex;
   flex-direction: row;
   max-width: 100%;
-
   width: 183.748px;
   height: 234.723px;
   flex-shrink: 0;
@@ -45,14 +50,12 @@ const TitleContainer = styled("div")`
   overflow-wrap: break-word;
   word-break: break-word;
   line-height: 22px;
-
   display: flex;
   width: 182.221px;
   height: 41.807px;
   flex-direction: column;
   justify-content: center;
   flex-shrink: 0;
-
   color: #FFF;
   text-align: center;
   font-family: Angkor;
@@ -60,36 +63,19 @@ const TitleContainer = styled("div")`
   font-style: normal;
   font-weight: 400;
   line-height: normal;
-  .angkor-regular {
-    font-family: "Angkor", serif;
-    font-weight: 400;
-    font-style: normal;
-  }
-`;
-
-const AuthorContainer = styled("div")`
-  
 `;
 
 const ArticleCard = ({ props }) => {
+  const randomColor = getRandomColor(); // Get random color for the background
   return (
-    <ArticleContainer >
-      <div style={{ position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+    <ArticleContainer bgColor={randomColor}>
+      <div style={{ position: "relative", display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
         <BoxContainer>
-            <TitleContainer>{props.article_title}</TitleContainer>
+          <TitleContainer>{props.article_title}</TitleContainer>
         </BoxContainer>
       </div>
     </ArticleContainer>
   );
 };
-
-// props:
-// article_title
-// article_url
-// article_byline
-// article_image
-// Idea: for different colors randomize between green, orange, blue, teal, yellow, pink
-// for the colors: we are taking it in as a parameter; it will be randomly generated in articleGrid and will be passed into articleCard as a parameter
-
 
 export default ArticleCard;
