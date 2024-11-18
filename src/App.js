ac98ed4f40d925e391654e282c4a8060298603e0
Evelyn Cho <91930713+ycho1908@users.noreply.github.com>
2024-11-17 22:26:00 -0800

    completed article card (#3)
    * created the articleCard templates/layout
    
    * added blur effect
    
    * updated the layout of articleCard
    
    * randomized bg color
    
    * softer glow fixing
    
    * color not random but in specific order;
    
    * added spacing for title, added authors, all-caps
    
    ---------
    
    Co-authored-by: maxfukuh4ra <maxfuku04@g.ucla.edu>
    Co-authored-by: Richelle Shim <richelleshim@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import ArticleGrid from "./components/ArticleGrid"
+import ArticleCard from "./components/ArticleCard";
 
 function App() {
+  const [data, setData] = useState(null);
   const [data, setData] = useState(null);
   
   useEffect(() => {
@@ -12,15 +13,25 @@ function App() {
       .then(res => res.json())
       .then(res => setData(res.data['article.aml'])); // Load "article.aml" from API
   }, []);
+    fetch("https://kerckhoff.dailybruin.com/api/packages/flatpages/rivalry-issue-24-25")
+      .then(res => res.json())
+      .then(res => setData(res.data['article.aml'])); // Load "article.aml" from API
+  }, []);
 
+  return (
   return (
     <div className="App">
       <Header />
+      <Header />
       Hello Daily Bruin!
-      <ArticleGrid />
-      <Footer/>
-
-
+      <Footer />
+      {/* TO TEST OUT THE ARTICLECARD COMPONENT */}
+      {data && data.articles.map((article, index) => (
+        <ArticleCard 
+          index={index}
+          props={article} // Pass the entire article object as `props`
+        />
+      ))}
     </div>
   );
 }
