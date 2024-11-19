d2447632310d4f0aed42604de620d3d1eeb067e7
sahilsheth05 <sahilnsheth@gmail.com>
2024-11-18 19:16:03 -0800

    Changing Header and Footer
    Changed header to match gradient in the Figma and changed footer to correct color. Also rearranged the header and footer components in App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
 import React, { useState, useEffect } from "react";
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import ArticleCard from "./components/ArticleCard";
 
 function App() {
   const [data, setData] = useState(null);
-  const [data, setData] = useState(null);
-  
+
   useEffect(() => {
-    fetch("https://kerckhoff.dailybruin.com/api/packages/flatpages/rivalry-issue-24-25")
-      .then(res => res.json())
-      .then(res => setData(res.data['article.aml'])); // Load "article.aml" from API
-  }, []);
-    fetch("https://kerckhoff.dailybruin.com/api/packages/flatpages/rivalry-issue-24-25")
-      .then(res => res.json())
-      .then(res => setData(res.data['article.aml'])); // Load "article.aml" from API
+    fetch(
+      "https://kerckhoff.dailybruin.com/api/packages/flatpages/rivalry-issue-24-25"
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res.data["article.aml"])); // Load "article.aml" from API
   }, []);
 
-  return (
+
   return (
     <div className="App">
       <Header />
-      <Header />
-      Hello Daily Bruin!
-      <Footer />
       {/* TO TEST OUT THE ARTICLECARD COMPONENT */}
-      {data && data.articles.map((article, index) => (
-        <ArticleCard 
-          index={index}
-          props={article} // Pass the entire article object as `props`
-        />
-      ))}
+      {data &&
+        data.articles.map((article, index) => (
+          <ArticleCard
+            index={index}
+            props={article} // Pass the entire article object as `props`
+          />
+        ))}
+      <Footer />
     </div>
   );
 }
