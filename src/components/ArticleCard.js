4f9e3c44f38a73b8c738d80d3285d15841346de6
Jennifer Lee <jenni.lee1212@gmail.com>
2024-11-21 12:44:48 -0800

    Updated images to iterate through the ones on the figma

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -10,6 +10,18 @@ const colorSequence = [
   "#CE9FD1",
 ];
 
+const imageSequence = [
+  "https://assets3.dailybruin.com/images/rivalry-issue-24-25/TJ-harden-bf449f4a94d9657a33589fe85f34cef5.jpg",
+  "https://assets3.dailybruin.com/images/rivalry-issue-24-25/Me-drano-f8453ed981aeb1b9883d57d56ec9f6f6.jpg",
+  "https://assets3.dailybruin.com/images/rivalry-issue-24-25/Ga-rbers-b07660ab2f33233ec8731f7fd68894c6.jpg",
+  "https://assets3.dailybruin.com/images/rivalry-issue-24-25/Sch-wes-b8551966a2621d5a2743f00bf90e119d.jpg",
+  "https://assets3.dailybruin.com/images/rivalry-issue-24-25/Keegan-Jones-cc6e7aaeb4363b4768b4f7acabee4ee8.jpg",
+  "https://assets3.dailybruin.com/images/rivalry-issue-24-25/deshaun-1ad40b99ce6a8c4593ba62dfd955e88e.jpg",
+  "https://assets3.dailybruin.com/images/rivalry-issue-24-25/Lo-ya-dee3d5f0ba95a0b2746ef77f166b66e8.jpg",
+  "https://assets3.dailybruin.com/images/rivalry-issue-24-25/Mat-avao-bb4076d7f212dbef28b07a60164a8654.jpg",
+  "https://assets3.dailybruin.com/images/rivalry-issue-24-25/Fe-mi-6ce857dabfe4293cb83b40d38ab840b1.jpg",
+];
+
 const ArticleContainer = styled("div")`
   // outermost container
   position: relative;
@@ -95,6 +107,7 @@ const TitleContainer = styled("div")`
   line-height: 22px;
   display: flex;
   padding: 5% 0% 0% 5%;
+  margin-bottom: 20px;
   width: 323px; /* updated for desktop size */
   height: 41.807px; /* updated for desktop size */
   flex-direction: column;
@@ -114,6 +127,7 @@ const TitleContainer = styled("div")`
 
   @media (max-width: 900px) {
     padding: 5% 0% 0% 0%;
+    margin-bottom: 10px;
     font-size: 12px;
     height: 41.807px;
     flex-direction: column;
@@ -172,11 +186,12 @@ const AuthorContainer = styled("div")`
 
 const ArticleCard = ({ props, index }) => {
   const bgColor = colorSequence[index % colorSequence.length]; // Cycle through colors in sequence
+  const articleImage = imageSequence[index % imageSequence.length] // Cycle through images in a sequence
   return (
     <ArticleContainer bgColor={bgColor}>
       <BoxContainer>
         <TitleContainer>{props.article_title}</TitleContainer>
-        <img src = {props.article_image} alt = "Article Image" />
+        <img src = {articleImage} alt = "Article Image" />
       </BoxContainer>
       <AuthorContainer>{props.article_byline}</AuthorContainer>
     </ArticleContainer>
